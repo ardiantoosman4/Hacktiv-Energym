a4e1c2d8e779dc03c3665b6237cb9b28c009828e
Ardianto <108719126+ardiantoosman4@users.noreply.github.com>
2023-09-26 17:01:14 +0700

    functional for register, login, and logout (done)

--- JS/navbar-ardi.js
+++ JS/navbar-ardi.js
@@ -1,21 +1,23 @@
 // ##########################################
 // untuk test set True untuk menampilkan username dg dropdown
 // set false untuk menampilkan Login
-localStorage.setItem("isLogin", false);
+// localStorage.setItem("isLogin", false);
+// localStorage.setItem("isLogin", null);
 // #########################################
 changeNavbar();
 
 function changeNavbar() {
   let liDynamic = document.getElementById("variable-link");
   if (localStorage.getItem("isLogin") === "true") {
-    console.log("JALANKAN USERNAME");
     liDynamic.innerHTML = `
                           <a href="#">Username</a>
                           <ul>
-                              <li><a href="#">My Profile</a></li>
-                              <li><a href="#">Logout</a></li>
+                              <li><a href="#" id = "profile-user">My Profile</a></li>
+                              <li><a href="#" id = "logout-user">Logout</a></li>
                           </ul>`;
+    listenLogout();
   } else {
+    localStorage.setItem("isLogin", false);
     liDynamic.innerHTML = `<a href="#" id = "open-pop-up-login">Login</a>`;
     generatePopUpLogin();
     generatePopUpRegister();
@@ -23,6 +25,8 @@ function changeNavbar() {
     listenOpenRegister();
     ListenCloseLogin();
     listenCloseRegister();
+    listenSubmitLogin();
+    listenSubmitRegister();
   }
 }
 
@@ -36,15 +40,15 @@ function generatePopUpLogin() {
                             <form action="#">
                                 <div class="login-data">
                                 <label>Email</label>
-                                <input type="email" required>
+                                <input type="email" id="login-email" required>
                                 </div>
                                 <div class="login-data">
                                 <label>Password</label>
-                                <input type="password" required>
+                                <input type="password" id="login-pass" required>
                                 </div>
                                 <div class="login-btn">
                                 <div class="inner"></div>
-                                <button type="submit">login</button>
+                                <button type="submit" id="submit-login">login</button>
                                 </div>
                                 <div class="signup-link">
                                 Don't have an account? <a href="#" id = "open-pop-up-register">Register now</a>
@@ -62,19 +66,19 @@ function generatePopUpRegister() {
                             <form action="#">
                                 <div class="register-data">
                                 <label>Email</label>
-                                <input type="email" required>
+                                <input type="email" id="register-email" required>
                                 </div>
                                 <div class="register-data">
                                 <label>Password</label>
-                                <input type="password" required>
+                                <input type="password" id="register-pass" required>
                                 </div>
                                 <div class="register-data">
                                 <label>Confirm Password</label>
-                                <input type="password" required>
+                                <input type="password" id="register-pass-confirm"required>
                                 </div>
                                 <div class="register-btn">
                                 <div class="inner"></div>
-                                <button type="submit">Register</button>
+                                <button type="submit" id="submit-register">Register</button>
                                 </div>
                             </form> `;
 }
@@ -118,6 +122,92 @@ function listenCloseRegister() {
     pCloseRegister.addEventListener("click", function () {
       let divContainer = document.getElementById("pop-up-register");
       divContainer.classList.toggle("display-register-container");
+    });
+  }
+}
+
+function listenSubmitLogin() {
+  let btnSubmit = document.getElementById("submit-login");
+  btnSubmit.addEventListener("click", function () {
+    let inputEmail = document.getElementById("login-email").value;
+    let inputPass = document.getElementById("login-pass").value;
+    let oldInputEmail = JSON.parse(localStorage.getItem("email"));
+    if (!oldInputEmail) {
+      oldInputEmail = [];
+    }
+    let oldInputPass = JSON.parse(localStorage.getItem("password"));
+    if (!oldInputPass) {
+      oldInputPass = [];
+    }
+    let isAnyMatch = false;
+
+    for (let i = 0; i < oldInputEmail.length; i++) {
+      let dbEmail = oldInputEmail[i];
+      let dbPass = oldInputPass[i];
+      if (dbEmail === inputEmail && dbPass === inputPass) {
+        isAnyMatch = true;
+        break;
+      }
+    }
+    if (isAnyMatch === true) {
+      localStorage.setItem("currentUser", inputEmail);
+      localStorage.setItem("isLogin", true);
+      let divContainer = document.getElementById("pop-up-login");
+      divContainer.classList.toggle("display-login-container");
+      changeNavbar();
+      window.location.reload();
+    } else {
+      alert("Please check inputed email and password");
+    }
+  });
+}
+
+function listenSubmitRegister() {
+  let btnSubmit = document.getElementById("submit-register");
+  btnSubmit.addEventListener("click", function () {
+    let inputEmail = document.getElementById("register-email").value;
+    let inputPass = document.getElementById("register-pass").value;
+    let inputPassConfirm = document.getElementById(
+      "register-pass-confirm"
+    ).value;
+
+    if (inputPass !== inputPassConfirm) {
+      alert("Password not match with password confirmation");
+      return;
+    }
+    let oldInputEmail = JSON.parse(localStorage.getItem("email"));
+    if (!oldInputEmail) {
+      oldInputEmail = [];
+    }
+
+    let oldInputPass = JSON.parse(localStorage.getItem("password"));
+    if (!oldInputPass) {
+      oldInputPass = [];
+    }
+    oldInputEmail.push(inputEmail);
+    oldInputPass.push(inputPass);
+    let newEmail = JSON.stringify(oldInputEmail);
+    let newPass = JSON.stringify(oldInputPass);
+    localStorage.setItem("email", newEmail);
+    localStorage.setItem("password", newPass);
+
+    let divContainer = document.getElementById("pop-up-register");
+    divContainer.classList.toggle("display-register-container");
+  });
+}
+
+function listenLogout() {
+  let aLogout = document.getElementById("logout-user");
+  if (aLogout) {
+    aLogout.addEventListener("click", function () {
+      if (confirm("Are You Sure?")) {
+        localStorage.setItem("currentUser", null);
+        localStorage.setItem("isLogin", false);
+        changeNavbar();
+        window.location.reload();
+      } else {
+        return;
+      }
     });
   }
 }
